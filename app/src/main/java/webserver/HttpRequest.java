fd7673dc983ad6530419b871c94c9cf19c900a54
Anthony Jo <yj.anthonyjo@gmail.com>
2024-10-30 17:01:08 +0900

    feat: HTTP Message Skeleton

---
+++ app/src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,13 @@
+package webserver;
+
+public class HttpRequest {
+    public String host;
+    public String method;
+    public String path;
+    public String version;
+    public String userAgent;
+    public String acceptLanguage;
+    public String query;
+    public String body;
+    public Integer port;
+}
---
+++ app/src/main/java/webserver/HttpResponse.java
@@ -0,0 +1,9 @@
+package webserver;
+
+public class HttpResponse {
+    public String version;
+    public Integer status;
+    public String body;
+    public String contentType;
+    public String contentLength;
+}
