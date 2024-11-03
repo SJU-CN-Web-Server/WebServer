9ccfc88581c8a8723915993d0fd936db2c907be4
Anthony Jo <yj.anthonyjo@gmail.com>
2024-11-03 18:14:24 +0900

    router 기능 추가

--- app/src/main/java/webserver/RouterHandler.java
+++ app/src/main/java/webserver/RouterHandler.java
@@ -1,25 +1,5 @@
 package webserver;
 
-// import java.net.Socket;
-// import java.nio.file.Path;
-// import java.nio.file.Paths;
-
-// import webserver.data.HttpRequest;
-// import webserver.data.HttpResponse;
-
-// public class RouterHandler extends HttpHandler{
-//     @Override
-//     public void process(HttpRequest request, HttpResponse response, Socket clientSocket) {
-//         request.absPath = getAbsPath(request);
-//         System.out.println("absPath: " + request.absPath);
-//     }
-
-//     String getAbsPath(HttpRequest request) {
-//         String basePath = "./";
-//         Path path = Paths.get(basePath, request.path == null ? "" : request.path, "hello.txt").toAbsolutePath();
-//         return path.toString();
-//     }
-// }
 import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
