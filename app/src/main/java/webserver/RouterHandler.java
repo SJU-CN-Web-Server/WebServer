7e811f65c0bb084e2f6a3e3b5235fd36a8708fae
Anthony Jo <yj.anthonyjo@gmail.com>
2024-11-05 23:53:17 +0900

    refactor: router - final

--- app/src/main/java/webserver/RouterHandler.java
+++ app/src/main/java/webserver/RouterHandler.java
@@ -3,14 +3,10 @@ package webserver;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import webserver.data.HttpRequest;
 import webserver.data.HttpResponse;
 public class RouterHandler extends HttpHandler {
-    private List<Map<String, String>> mappings = new ArrayList<>();
 
     @Override
     public void process(HttpRequest request, HttpResponse response, Socket clientSocket) {
@@ -20,33 +16,8 @@ public class RouterHandler extends HttpHandler {
     
     String getAbsPath(HttpRequest request) {
         String basePath = System.getProperty("user.dir"); 
-        String relativePath = stripPatternFromPath(request.path, getUrlPattern(request.path));
-        Path path = Paths.get(basePath, relativePath).toAbsolutePath();
+        Path path = Paths.get(basePath, request.path).toAbsolutePath();
         return path.toString();
-    }
-
-    // URL 패턴 부분을 제거하여 남은 경로만 반환
-    private String stripPatternFromPath(String urlPath, String urlPattern) {
-        // URL 패턴 부분을 제거하여 남은 경로만 반환
-        if (urlPattern.endsWith("/*")) {
-            String basePattern = urlPattern.replace("/*", "");
-            if (urlPath.startsWith(basePattern)) {
-                return urlPath.substring(basePattern.length());
-            }
-        }
-        return urlPath;  // 패턴이 없는 경우 원본 URL 경로 반환
-    }
-    
-    
-    // URL 경로에 해당하는 URL 패턴을 찾는 메서드
-    private String getUrlPattern(String urlPath) {
-        for (Map<String, String> mapping : mappings) {
-            String urlPattern = mapping.get("urlPattern");
-            if (urlPath.matches(urlPattern.replace("*", ".*"))) {
-                return urlPattern;
-            }
-        }
-        return "";  // 매칭되는 패턴이 없을 경우 빈 문자열 반환
     }
 
 }
