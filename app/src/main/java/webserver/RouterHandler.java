b83df58ca0e5ec53830d800342af7851d8d1d853
Anthony Jo <yj.anthonyjo@gmail.com>
2024-11-05 23:51:24 +0900

    refactor: router - delete unused code(commented)

--- app/src/main/java/webserver/RouterHandler.java
+++ app/src/main/java/webserver/RouterHandler.java
@@ -12,38 +12,14 @@ import webserver.data.HttpResponse;
 public class RouterHandler extends HttpHandler {
     private List<Map<String, String>> mappings = new ArrayList<>();
 
-    // 매핑을 설정하는 생성자 (예제에서 하드코딩, 외부 설정 파일을 읽는 경우로도 변경 가능)
-    // public RouterHandler() {
-        // URL 패턴과 파일 시스템 경로 매핑을 하드코딩 예제로 설정
-        // loadMappings();
-        // mappings.add(Map.of("urlPattern", "/tmp/*", "fileSystemPath", "/var/www/html/tmp"));
-        // mappings.add(Map.of("urlPattern", "/docs/*", "fileSystemPath", "/var/www/html/docs"));
-    // }
-
     @Override
     public void process(HttpRequest request, HttpResponse response, Socket clientSocket) {
         request.absPath = getAbsPath(request);
         System.out.println("absPath: " + request.absPath);
     }
-    // private void loadMappings() {
-    //     ObjectMapper objectMapper = new ObjectMapper();
-    //     try (InputStream input = getClass().getClassLoader().getResourceAsStream("location.json")) {
-    //         if (input == null) {
-    //             // throw new IllegalStateException("Could not find location.json file in resources");
-    //             System.out.println("Could not find location.json file in resources, use default mappings");
-    //             return;
-    //         }
-    //         // JSON 파일을 Java 객체로 변환
-    //         Map<String, List<Map<String, String>>> jsonMap = objectMapper.readValue(input, Map.class);
-    //         mappings = jsonMap.get("mappings");
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //         throw new RuntimeException("Failed to load mappings from location.json", e);
-    //     }
-    // }
     
     String getAbsPath(HttpRequest request) {
-        String basePath = System.getProperty("user.dir"); //getFileSystemPath(request.path);  // 매핑된 파일 시스템 경로 가져오기
+        String basePath = System.getProperty("user.dir"); 
         String relativePath = stripPatternFromPath(request.path, getUrlPattern(request.path));
         Path path = Paths.get(basePath, relativePath).toAbsolutePath();
         return path.toString();
@@ -72,22 +48,5 @@ public class RouterHandler extends HttpHandler {
         }
         return "";  // 매칭되는 패턴이 없을 경우 빈 문자열 반환
     }
-
-    // URL 경로에 맞는 파일 시스템 경로를 반환
-    // private String getFileSystemPath(String urlPath) {
-    //     for (Map<String, String> mapping : mappings) {
-    //         String urlPattern = mapping.get("urlPattern");
-    //         String fileSystemPath = mapping.get("fileSystemPath");
-
-    //         String regre = urlPattern.replace("*", ".*");
-    //         // urlPattern의 '*'을 정규식 '.*'로 변환하여 매칭 검사
-    //         // System.out.println("regre: " + regre);
-
-    //         if (urlPath.matches(regre)) {
-    //             return fileSystemPath;
-    //         }
-    //     }
-    //     return System.getProperty("user.home") + File.separator + "html"; // 기본 경로 반환 (필요시 수정 가능)
-    // }
 
 }
